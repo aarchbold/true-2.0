377e7531bd938283504bc9056448b093de523a5d
Andrew Archbold <pulsegrenade@gmail.com>
2019-06-30 17:19:34 -0700

    #time 2h 0m adding animation engine

--- site/static/js/app.js
+++ site/static/js/app.js
@@ -48,3 +48,32 @@ function isElementInViewport (el) {
 
     return (vertInView && horInView);
 }
+setHeroHeight = debounce(function() { 
+    $heroContainer = $('#heroStage');
+    $sun = $('.hero-stage__sun',$heroContainer);
+    $fogs = $('.hero-stage__fog',$heroContainer);
+    $header = $('.hero-stage__header',$heroContainer)
+    $heroContainer.height($(window).height());
+    startAnimations = function() {
+        $sun.addClass('-animate');
+        $fogs.addClass('-animate');
+        $heroContainer.addClass('-animate');
+    }
+    startHeaderTextAnimation = function() {
+        $header.addClass('-animate');
+    }
+
+    // start animations
+    setTimeout(function() {
+        startAnimations();
+    },1000)
+    setTimeout(function() {
+        startHeaderTextAnimation();
+    },1500)
+},250);
+
+
+$(function() {
+    setHeroHeight();
+    window.addEventListener('resize', setHeroHeight);
+});
