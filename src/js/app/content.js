a9b2d3d7931912ae394b474470ca1d32087147ca
Andrew Archbold <pulsegrenade@gmail.com>
2019-07-15 17:18:27 -0700

    #time 1h 0m adding share links

--- src/js/app/content.js
+++ src/js/app/content.js
@@ -1,7 +1,57 @@
-$.fn.bounceIn = function(){
-    
+var getWindowOptions = function() {
+    var width = 500;
+    var height = 450;
+    var left = (window.innerWidth / 2) - (width / 2);
+    var top = (window.innerHeight / 2) - (height / 2);
+  
+    return [
+      'resizable,scrollbars,status',
+      'height=' + height,
+      'width=' + width,
+      'left=' + left,
+      'top=' + top,
+    ].join();
+  };
+
+var shareOnFacebook = function() {
+    var fbBtn = $('.facebook-share');
+    var title = encodeURIComponent('Welcome to our Happy Valley :: True');
+    var shareUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + location.href + '&title=' + title;
+    fbBtn.href = shareUrl; // 1
+
+    fbBtn.click(function(e) {
+        e.preventDefault();
+        var win = window.open(shareUrl, 'ShareOnFb', getWindowOptions());
+        win.opener = null; // 2
+    });
 }
 
+var shareOnTwitter = function() {
+    var tweetBtn = $('.twitter-share');
+    var title = encodeURIComponent('Welcome to our Happy Valley :: True');
+    var shareUrl = 'https://twitter.com/intent/tweet?text=' + title + '&url=' + location.href;
+    tweetBtn.href = shareUrl; // 1
+
+    tweetBtn.click(function(e) {
+        e.preventDefault();
+        var win = window.open(shareUrl, 'ShareOnTwitter', getWindowOptions());
+        win.opener = null; // 2
+    });
+}
+
+var initCopyToClip = function() {
+    var $input = $('#clipboardCopy');
+    var $button = $('#copyUrl');
+
+    $input.attr('value',window.location.href);
+
+    $button.click(function(e) {
+        e.preventDefault();
+        $input.select();
+        document.execCommand('copy');
+        $button.text('Link Copied!');
+    });
+}
 
 
 $(function() {
@@ -11,6 +61,10 @@ $(function() {
             scrollTop: ($('#joinUs').offset().top)
         },1000);
     })
+
+    shareOnFacebook();
+    shareOnTwitter();
+    initCopyToClip();
 
     $(window).on('DOMContentLoaded load resize scroll', function() {
 
