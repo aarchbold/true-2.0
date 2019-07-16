6d8e82838d104c8973a5425816690468227fd8f1
Andrew Archbold <pulsegrenade@gmail.com>
2019-07-15 22:29:35 -0700

    #time 2h 0m animating the hero section and adding new assets. Also added the sticky nav

--- site/static/js/app.js
+++ site/static/js/app.js
@@ -166,12 +166,15 @@ initHero = debounce(function() {
     $heroContainer = $('#heroStage');
     $mainSite = $('.main-site__container');
     $sun = $('.hero-stage__sun',$heroContainer);
+    $shine = $('.main-site__shine',$mainSite);
     $fogs = $('.hero-stage__fog',$mainSite);
     $header = $('.hero-stage__header',$heroContainer);
     $clouds = $('.animated-cloud',$heroContainer);
     $birds = $('.animated-bird',$mainSite);
     $baloon1 = $('.main-site__balloon1',$heroContainer);
     $baloon2 = $('.main-site__balloon2',$heroContainer);
+    $nav = $('.topnav-container');
+    $navInner = $('.navigation-inner');
     var $window = $(window);
     
     // set the fog to the screen height
@@ -179,7 +182,8 @@ initHero = debounce(function() {
     
     startAnimations = function() {
         $sun.addClass('-animate');
-        $fogs.addClass('-animate');
+        $shine.addClass('-animate');
+        //$fogs.addClass('-animate');
         $heroContainer.addClass('-animate');
         $clouds.addClass('-animate');
         $birds.addClass('-animate');
@@ -194,8 +198,17 @@ initHero = debounce(function() {
       var scrollTop = $window.scrollTop();
       if (scrollTop > 20) {
         $header.addClass('-fade');
+        $nav.addClass('-show');
       } else {
         $header.removeClass('-fade');
+        $nav.removeClass('-show');
+      }
+      if (scrollTop > 400) {
+        $navInner.addClass('-compact');
+        $heroContainer.hide();
+      } else {
+        $navInner.removeClass('-compact');
+        $heroContainer.show();
       }
     });
     // start animations
@@ -204,7 +217,7 @@ initHero = debounce(function() {
     },1000)
     setTimeout(function() {
         startHeaderTextAnimation();
-    },2500)
+    },1250)
     setTimeout(function() {
         //make body scrollable 
         $('body').removeClass('-static');
@@ -231,64 +244,14 @@ initHero = debounce(function() {
 //   // $(window).scrollTo('23%', 1200);
 // }
 
-$(function() {
-    initHero();
-    // window.addEventListener('resize', setHeroHeight);
-    // var scene = document.getElementById('scene');
-    // var parallaxInstance = new Parallax(scene);
-    // console.log(parallaxInstance)
-    // $('.main-site__container').paroller();
-    
-    
+
+$(window).on('load', function (e) {
+  // executes when complete page is fully loaded, including all frames, objects and images
+  // fade out the preload spinner.
+  $('.preloader-shim').addClass('-animate');
+
+  initHero();    
+  if ($(window).width() > 800) {
     $('[data-scroll-speed]').moveIt();
-    
-    
-    // $.scrollSpeed(100, 800, 'easeOutCubic');
-
-
-    // $.scrollify({
-    //   section : '.scroll-me',
-    //   sectionName : 'section-name',
-    //   setHeights: false,
-    //   //standardScrollElements: '.no-scroll',
-    //   scrollSpeed: 1600,
-    //   touchScroll: false,
-    //   offset: 0
-    // });
-
-    $('#scroll-test-1').click(function(e) {
-      e.preventDefault();
-      //$.scrollTo($("#sharingSection"), 1200);
-      $(window).scrollTo('23%', 1200)
-      // $(window).scrollTo({top:'50%'}, 800);
-    });
-    $('#scroll-test-2').click(function(e) {
-      e.preventDefault();
-      //$.scrollTo($("#sharingSection"), 1200);
-      $(window).scrollTo('44%', 1200)
-      // $(window).scrollTo({top:'50%'}, 800);
-    });
-    $('#scroll-test-3').click(function(e) {
-      e.preventDefault();
-      //$.scrollTo($("#sharingSection"), 1200);
-      $(window).scrollTo('65%', 1200)
-      // $(window).scrollTo({top:'50%'}, 800);
-    });
-
-
-
-    // // scroll fun
-    // var lastScrollTop = 0;
-    // // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
-    // window.addEventListener('scroll', function(){ // or window.addEventListener("scroll"....
-    //   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-    //   if (st > lastScrollTop){
-    //       scrollDirection = 'down';
-    //   } else {
-    //     scrollDirection = 'up';
-    //   }
-    //   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-    // }, false);
-
-    //window.addEventListener('scroll', throttle(scrollToSection, 1000));
-});
+  }
+})
