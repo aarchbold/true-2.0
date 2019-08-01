f59821eeae5f410248edbc4530453fd8fb5428fc
Andrew Archbold <pulsegrenade@gmail.com>
2019-07-31 23:38:48 -0700

    #time 3h 0m landing page revisions and VIP logic

--- src/js/app/content.js
+++ src/js/app/content.js
@@ -1,59 +1,3 @@
-var getWindowOptions = function() {
-    var width = 500;
-    var height = 450;
-    var left = (window.innerWidth / 2) - (width / 2);
-    var top = (window.innerHeight / 2) - (height / 2);
-  
-    return [
-      'resizable,scrollbars,status',
-      'height=' + height,
-      'width=' + width,
-      'left=' + left,
-      'top=' + top,
-    ].join();
-  };
-
-var shareOnFacebook = function() {
-    var fbBtn = $('.facebook-share');
-    var title = encodeURIComponent('Check out this new social network I just found—you can make private threads with your favorite people (like me 😄). Sign up so we can be the first to try it:');
-    var shareUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + location.href + '&title=' + title;
-    fbBtn.href = shareUrl; // 1
-
-    fbBtn.click(function(e) {
-        e.preventDefault();
-        var win = window.open(shareUrl, 'ShareOnFb', getWindowOptions());
-        win.opener = null; // 2
-    });
-}
-
-var shareOnTwitter = function() {
-    var tweetBtn = $('.twitter-share');
-    var title = encodeURIComponent('Check out this new social network I just found—you can make private threads with your favorite people (like me 😄). Sign up so we can be the first to try it:');
-    var shareUrl = 'https://twitter.com/intent/tweet?text=' + title + '&url=' + location.href;
-    tweetBtn.href = shareUrl; // 1
-
-    tweetBtn.click(function(e) {
-        e.preventDefault();
-        var win = window.open(shareUrl, 'ShareOnTwitter', getWindowOptions());
-        win.opener = null; // 2
-    });
-}
-
-var initCopyToClip = function() {
-    var $input = $('#clipboardCopy');
-    var $button = $('#copyUrl');
-
-    $input.attr('value',window.location.href);
-
-    $button.click(function(e) {
-        e.preventDefault();
-        $input.select();
-        document.execCommand('copy');
-        $button.text('Link Copied!');
-    });
-}
-
-
 $(function() {
     $('.go-to-footer').click(function(e) {
         e.preventDefault();
@@ -66,10 +10,6 @@ $(function() {
     var $firstVideo = $('#firstVideo');
     $firstVideo[0].playbackRate = 0.65;
     $firstVideo[0].play();
-
-    shareOnFacebook();
-    shareOnTwitter();
-    initCopyToClip();
 
     $(window).on('DOMContentLoaded load resize scroll', function() {
 
--- src/js/app/waitlist.js
+++ src/js/app/waitlist.js
@@ -1,13 +1,97 @@
+var getWindowOptions = function() {
+    var width = 500;
+    var height = 450;
+    var left = (window.innerWidth / 2) - (width / 2);
+    var top = (window.innerHeight / 2) - (height / 2);
+  
+    return [
+      'resizable,scrollbars,status',
+      'height=' + height,
+      'width=' + width,
+      'left=' + left,
+      'top=' + top,
+    ].join();
+  };
+
+var shareOnFacebook = function(inviteCode) {
+    var url = [location.protocol, '//', location.host, location.pathname].join('');
+    var fbBtn = $('.facebook-share');
+    var title = encodeURIComponent('Check out this new social network I just found—you can make private threads with your favorite people (like me 😄). Sign up so we can be the first to try it:');
+    var shareUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + url + inviteCode + '&title=' + title;
+    fbBtn.href = shareUrl; // 1
+
+    fbBtn.click(function(e) {
+        e.preventDefault();
+        var win = window.open(shareUrl, 'ShareOnFb', getWindowOptions());
+        win.opener = null; // 2
+    });
+}
+
+var shareOnTwitter = function(inviteCode) {
+    var url = [location.protocol, '//', location.host, location.pathname].join('');
+    var tweetBtn = $('.twitter-share');
+    var title = encodeURIComponent('Check out this new social network I just found—you can make private threads with your favorite people (like me 😄). Sign up so we can be the first to try it:');
+    var shareUrl = 'https://twitter.com/intent/tweet?text=' + title + '&url=' + url + inviteCode;
+    tweetBtn.href = shareUrl; // 1
+
+    tweetBtn.click(function(e) {
+        e.preventDefault();
+        var win = window.open(shareUrl, 'ShareOnTwitter', getWindowOptions());
+        win.opener = null; // 2
+    });
+}
+
+var initCopyToClip = function(inviteCode) {
+    var $input = $('#clipboardCopy');
+    var $button = $('#copyUrl');
+    var url = [location.protocol, '//', location.host, location.pathname].join('');
+
+    $input.attr('value',url + inviteCode);
+
+    $button.click(function(e) {
+        e.preventDefault();
+        $input.select();
+        document.execCommand('copy');
+        $button.text('Link Copied!');
+    });
+}
+
+var initEmailShare = function(inviteCode) {
+    var $button = $('.button-send-email');
+    var url = [location.protocol, '//', location.host, location.pathname].join('');
+
+    $button.attr('href','mailto:?subject=Sign up to try True with me&body=Hi, I just signed up to try this new social network called True. It lets you make private threads with your favorite people (like me 😄). Sign up so we can try it together: '+url+inviteCode)
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
 var handleWaitlist = function() {
     var $modal = $('.modal-waitlist');
+    var referrerCode = getParameterByName('inviteCode') || null;
     var $closeModal = $('#closeOverlay');
     var $modalSpinner = $('.modal-waitlist__preloader');
-    var $formInput = $('.footer-input input');
+    var $formName = $('.footer-input #trueName');
+    var $formInput = $('.footer-input #trueEmail');
     var $formSubmit = $('.footer-input .footer-signup');
     var emailPattern = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
 
     $formInput.keyup(function() {
-        if (emailPattern.test($formInput.val())) {
+        if ($formName.val() !== '' && emailPattern.test($formInput.val())) {
+            $formSubmit.removeClass('-disabled')
+        } else {
+            $formSubmit.addClass('-disabled')
+        }
+    })
+    $formName.keyup(function() {
+        if ($formName.val() !== '' && emailPattern.test($formInput.val())) {
             $formSubmit.removeClass('-disabled')
         } else {
             $formSubmit.addClass('-disabled')
@@ -15,12 +99,17 @@ var handleWaitlist = function() {
     })
 
     $formSubmit.click(function(e) {
+        var postData = {
+            email: $formInput.val(),
+            full_name: $formName.val(),
+            referrer_code: referrerCode
+        }
         e.preventDefault();
         if (!$formSubmit.hasClass('-disabled')) {
             $.ajax({
                 url: 'https://us-central1-trueappco-website.cloudfunctions.net/waitlist',
                 type: 'POST',
-                data: {email: $formInput.val()},
+                data: postData,
                 dataType: 'json',
                 cache: false,
                 beforeSend: function() {
@@ -31,18 +120,27 @@ var handleWaitlist = function() {
                 error: function(xhr, ajaxOptions, thrownError) { // if error occured
                 },
                 complete: function(data) {
+                    let inviteCode = '';
                     console.log(data.responseJSON);
-                    setTimeout(function() {
-                        $modalSpinner.fadeOut();
-                        $formInput.val('');
-                    },1000)
+                    if (data.responseJSON && data.responseJSON.inviteCode) {
+                        inviteCode = data.responseJSON.inviteCode;
+                    }
+                    shareOnFacebook(inviteCode);
+                    shareOnTwitter(inviteCode);
+                    initCopyToClip(inviteCode);
+                    initEmailShare(inviteCode);
+                    $modalSpinner.fadeOut();
+                    $formInput.val('');
+                    $formName.val('');
                 }
             });
         }
     })
 
     $modal.click(function(e) {
-        $modal.fadeOut();
+        if(e.target == this){
+            $modal.fadeOut();
+        }
     })
     $closeModal.click(function(e) {
         e.preventDefault();
