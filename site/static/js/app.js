d6a57c64acafb117a805d683e62a00933173d3fd
Andrew Archbold <pulsegrenade@gmail.com>
2019-09-30 23:23:10 -0700

    #time 2h 0m changes from Bret and making the FAQs open and close more smoothly

--- site/static/js/app.js
+++ site/static/js/app.js
@@ -136,21 +136,47 @@ $(function() {
         // }
     }); 
 });
+function setAnserHeights() {
+    var $answers = $('.faq-questions__item-anwser');
+
+    // get height of the answers
+    setTimeout(function() {
+        $answers.each(function(i,e) {
+            console.log($(e).height())
+            $(e).height($(e).outerHeight());
+            $(e).hide();
+        })
+    },10)
+}
+
 $.fn.handleFAQ = function() {
-    var $context = $(this);
-    var $questions = $('.faq-questions__item', $context);
+    var $context = $(this),
+    $questions = $('.faq-questions__item', $context);
+
+    $context.css('opacity',1);
+
     $questions.each(function(i,e) {
         $('.faq-questions__item-question',$(this)).click(function() {
-            $('.faq-questions__item-anwser',$(this).parent()).toggleClass('-open');
+            if ($('.faq-questions__item-control',$(this)).hasClass('-open')) {
+                $('.faq-questions__item-anwser',$(this).parent()).slideUp(100);
+            } else {
+                $('.faq-questions__item-anwser',$(this).parent()).slideDown(250);
+            }
             $('.faq-questions__item-control',$(this)).toggleClass('-open');
         })
     })
 }
 
-$(function() {
+
+$(window).on('load', function(){
     if ($('.faq-questions').length > 0) {
+        setAnserHeights();
         $('.faq-questions').handleFAQ();
     }
+});
+
+$(window).on('resize', function(){
+    // setAnserHeights();
 });
 function getParam(name) {
     SCH = document.location.search;
