1d79910d70a8332e8596b3621770a88f0e63d5a4
Andrew Archbold <pulsegrenade@gmail.com>
2019-11-12 22:56:54 -0800

    #time 4h 0m adding sharing and getting the blog post server side rendered

--- src/js/app/sharing.js
+++ src/js/app/sharing.js
@@ -1,5 +1,5 @@
 var shareBlogOnTwitter = function(message) {
-    var url = [location.protocol, '//', location.host, location.pathname].join('');
+    var url = window.location.href;
     var tweetBtn = $('.twitter-share');
     var title = message;
     var shareUrl = 'https://twitter.com/intent/tweet?text=' + title + '&url=' + url;
@@ -13,15 +13,30 @@ var shareBlogOnTwitter = function(message) {
 }
 
 var shareBlogOnLinkedIn = function(message) {
-    var url = [location.protocol, '//', location.host, location.pathname].join('');
+    var url = window.location.href;
     var linkedInBtn = $('.linkedin-share');
     var title = message;
-    var shareUrl = 'https://www.linkedin.com/sharing/share-offsite/?url' + url;
+    var shareUrl = 'https://www.linkedin.com/sharing/share-offsite/?url=' + url;
     linkedInBtn.href = shareUrl; // 1
 
     linkedInBtn.click(function(e) {
         e.preventDefault();
         var win = window.open(shareUrl, 'ShareOnLinkedIn', getWindowOptions());
+        win.opener = null; // 2
+    });
+}
+
+var shareBlogOnReddit = function(message) {
+    var url = window.location.href;
+    var redditBtn = $('.reddit-share');
+    var title = 'Check out this blog post at trytrue.com';
+    var shareUrl = 'http://www.reddit.com/submit?&url=' + encodeURI(url) + '&title='+ encodeURI(title);
+    redditBtn.href = shareUrl; // 1
+
+    redditBtn.click(function(e) {
+        // alert(encodeURI(shareUrl));
+        e.preventDefault();
+        var win = window.open(encodeURI(shareUrl), 'ShareOnReddit', getWindowOptions());
         win.opener = null; // 2
     });
 }
